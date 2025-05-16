2ed4e22acc73e7b30873ac504f548fed4f442ddf
Mathis Burger <handymathis@gmail.com>
2025-05-16 13:19:08 +0200

    feat: Added todo instructions

--- src/controller/expense/expense.rs
+++ src/controller/expense/expense.rs
@@ -1,11 +1,25 @@
-pub async fn request_expense() {}
+pub async fn request_expense() {
+    // Create expense with status "REQUEST"
+}
 
-pub async fn get_expense() {}
+pub async fn get_expense() {
+    // Gets the expense with all images
+}
 
-pub async fn get_expenses() {}
+pub async fn get_expenses() {
+    // Gets all expenses without images
+}
 
-pub async fn add_images_to_expense() {}
+pub async fn add_images_to_expense() {
+    // Add images to expense
+}
 
-pub async fn deny_expense() {}
+pub async fn deny_expense() {
+    // Set status to denied
+}
 
-pub async fn accept_expense() {}
+pub async fn accept_expense() {
+    // Set status to accepted
+    // Create both transactions
+    // Add expense to budget
+}
